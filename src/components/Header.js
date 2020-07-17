5eb40b6a1f2a474ff668e1de6a60f1c5a2556f27
IvanGuculyak <65493831+IvanGuculyak@users.noreply.github.com>
2020-07-18 01:03:34 +0300

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+
+            <header className="header">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQgY0KmCzk8uzhb4dkutVvG1B9Gdj6uFMp2qw&usqp=CAU" />
+            </header>
+
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div><img src="https://i.imgur.com/RRUe0Mo.png" /></div>
+            <div>ava + discription</div>
+            <div>My posts
+                <div>New post</div>
+                <div>Post 1</div>
+                <div>Post 2</div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
